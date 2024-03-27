f118c0c646f92edd32f102d568524b2c66801318
ZakGmd <Ghoumidatezakaria@gmail.com>
2024-03-27 03:08:36 +0000

    adding google sign in ui to our login component

--- components/ui/login.tsx
+++ components/ui/login.tsx
@@ -11,14 +11,27 @@ function LoginButton() {
     );
   }
 
+function GoogleLoginButton(){
+
+    return(
+        <button className="w-full h-12 flex items-center justify-center self-stretch gap-3 p-[14px] bg-gradient-to-br rounded-3xl from-white/5  to-gray-100/0 text-gray-100 text-[18px] font-normal leading-5 tracking-[-0.13px] ring-white/15 ring-1">Continue With Google  <Image src={"Gicon.svg"} alt={"arrow right icon svg "} width={20} height={20}  /> </button> 
+    )
+}
+
 export default function Login(){
 
 
     return(
         <form className="w-[520px] shadow-xl ring-white/15 ring-1 isolate rounded-xl bg-white/5  p-[40px] flex flex-col items-center justify-center ">
-            <div className="flex flex-col items-center gap-8 w-full ">
-                <div className="text-[28px] text-center leading-5 font-bold text-slate-100 tracking-[-0.12px] mb-9">Sign in</div>
-                <div className="w-full flex flex-col gap-4 items-start">
+            <div className="flex flex-col items-center gap-6 w-full ">
+                <div className="text-[28px] text-center leading-5 font-semibold text-slate-100 tracking-[-0.12px] mb-4 ">Sign in</div>
+                <GoogleLoginButton/>
+                <div className="flex items-center gap-4 ">
+                    <div className="w-[140px] h-[0.5px] bg-gray-400/30"></div>
+                    <div className="text-[16px] leading-5  font-normal text-slate-50  ">Or</div>
+                    <div className="w-[140px] h-[0.5px] bg-gray-400/30"></div>
+                </div>
+                <div className="w-full flex flex-col gap-4 items-start mt-[-8px]">
                   <label className="text-[16px] leading-5 tracking-[-0.12px] font-normal text-slate-100" htmlFor="password">Email</label>
                    <input
                     className="bg-transparent w-full rounded-md border border-gray-200/10 p-[14px] text-sm outline-none placeholder:text-gray-400/30 text-slate-50"
@@ -42,6 +55,7 @@ export default function Login(){
                     minLength={6}
                     /> 
                 </div>
+                <div className="text-[14px] leading-3 tracking-[-0.13px] underline text-start w-full text-slate-50/30">Forgot password ?</div>
                 <LoginButton/>
             </div>
         </form>
