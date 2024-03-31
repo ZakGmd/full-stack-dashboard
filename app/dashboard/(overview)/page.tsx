432fa676d472d277245a7c95bd239f4b7b682737
ZakGmd <Ghoumidatezakaria@gmail.com>
2024-03-31 05:56:48 +0000

    finishing theui of recent activity

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { Lato } from "next/font/google" ;
+import Link from "next/link";
 
 const lato = Lato({
     weight: ["300" ,"400" ,"700"] ,
@@ -76,8 +77,27 @@ export default function Page(){
                       </div>
                     </div>
                     <div className="col-span-1  inline-flex flex-col gap-3 items-start px-3">
-                      <div className="text-[22px] font-semibold leading-normal text-white tracking-[-0.12px]">Recent Transaction</div>
-                      <div className="h-[270px] w-full  bg-gradient-to-b from-white/5 from-2% to-white/10 ring-1 ring-white/15  rounded-[8px] "></div>
+                      <div className="text-[22px] font-semibold leading-normal text-white tracking-[-0.12px]">Recent activity</div>
+                      <div className="h-[270px] py-4 px-3 inline-flex gap-4 flex-col items-start w-full  bg-gradient-to-b from-white/5 from-2% to-white/10 ring-1 ring-white/15  rounded-[8px] ">
+                        <div className="text-[13px] text-white/90 leading-none tracking-[-0.12px]">Track project progress</div>
+                        <div className="w-full h-[0.5px] bg-white/10"></div>
+                        <div className="flex flex-col gap-3">
+                            <div className="text-[12px] leading-3 font-light tracking-[-0.12px] text-white/70">Today</div>
+                            <div className="flex items-start gap-3">
+                                <div className=" w-8 h-8 rounded-full bg-gradient-to-b from-white/0 to-white/5 ring-1 ring-white/10 flex shrink-0 "></div>
+                                <div className="flex flex-col items-start gap-1">
+                                    <div className="text-[14px] leading-5 tracking-[-0.12px] font-normal text-white max-w-[280px]  ">
+                                        <Link className="text-[#0052CC] font-medium" href={""}>Zakaria Ghoumidate </Link>
+                                         changed the status to In Progress on 
+                                        <Link className="  text-[#0052CC]" href={""}>
+                                            <Image src={"../checkIcon.svg"} alt={"check blue icon svg"} width={12} height={12} className=" align-middle inline-flex mx-1 "/>Zak-2asdsadadad
+                                        </Link>
+                                    </div>
+                                    <div  className="text-[12px]  leading-3 tracking-[-0.12px] font-normal text-gray-400">4 minutes ago</div>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
                     </div>
 
                 </div>
