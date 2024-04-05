36055b574d5da5fb601d45a62f338d93b9a14cc5
ZakGmd <Ghoumidatezakaria@gmail.com>
2024-04-05 23:01:33 +0000

    adjusting the ui of the ticket card , it looks good

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -65,21 +65,34 @@ export default function Page(){
                 <div className=" grid grid-cols-3 items-start gap-4">
                     <div className="col-span-2 inline-flex flex-col items-start gap-3  ">
                       <div className="text-[22px] font-semibold leading-normal text-white tracking-[-0.12px] ">Tickets</div>
-                      <div className="inline-flex items-center gap-3">
-                  <div className="px-2 py-3 bg-gradient-to-b from-white/5 from-2% to-white/10 ring-1 ring-white/15  rounded-[8px] min-w-[270px] max-w-[270px]  flex flex-col gap-9 ">
-                    <div className="flex items-center justify-between shrink-0 ">
+                      <div className="inline-flex items-start gap-3">
+                        <div className="px-2 py-3 bg-gradient-to-b  from-white/5 from-2% to-white/10 ring-1 ring-white/15  rounded-[8px] min-w-[270px] max-w-[270px]  flex flex-col gap-4">
+                         <div className="flex items-center justify-between shrink-0 ">
                         <div className=" inline-flex items-center gap-1 ">
                             <Image src={"../dots-6.svg"} alt={""} height={18} width={18}/>
                             <div className={` px-[4px] py-[3px] bg-white/15 ring-1 ring-white/5 text-center text-[11px] flex items-center font-[400] rounded text-slate-100 tracking-[-0.13px] leading-3 `}>TO DO</div>
                         </div>
                         <Image src={"../three-dots.svg"} alt={""} height={20} width={20} className="cursor-pointer"/>
-                    </div>
-                    <div className="inline-flex items-center gap-1 pt-2 px-1 border-t border-t-white/10 w-full  ">
+                         </div>
+                         <div className="w-full  bg-gradient-to-b px-2 py-3  from-white/20 from-2% to-white/10 ring-1 ring-white/25 rounded-lg flex flex-col gap-6 cursor-grab">
+                            <div className="flex items-center justify-between">
+                                <div className="text-[14px] leading-3 tracking-[-0.12px] font-normal text-white">hey</div>
+                                <Image src={"../three-dots.svg"} alt={""} width={20} height={20}/>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="inline-flex items-center gap-1">
+                                    <Image src={"../checkIcon.svg"} alt="check icon blue svg" width={14} height={14} />
+                                    <span className="text-[12px] leading-normal tracking-[-0.12px] font-normal text-gray-400">Zak-1</span>
+                                </div>
+                                <Image src={"../mediumIcon.svg"} width={18} height={18} alt=" 2 bars icon svg orange"/>
+                            </div>
+                         </div>
+                         <div className="inline-flex items-center gap-1 pt-2 px-1 border-t border-t-white/10 w-full  ">
                         <Image src={"../plus-circle.svg"} alt={""} height={18} width={18} />
                         <div className="text-[12px] text-slate-50 leading-tight tracking-[-0.12px]">Create</div>
                     </div>
-                  </div>
-                  <div className="px-2 py-3 bg-gradient-to-b from-white/5 from-2% to-white/10 ring-1 ring-white/15  rounded-[8px] min-w-[270px] max-w-[270px]  flex flex-col gap-9 ">
+                        </div>
+                        <div className="px-2 py-3 bg-gradient-to-b from-white/5 from-2% to-white/10 ring-1 ring-white/15  rounded-[8px] min-w-[270px] max-w-[270px]  flex flex-col gap-9 ">
                     <div className="flex items-center justify-between shrink-0 ">
                         <div className=" inline-flex items-center gap-1 ">
                             <Image src={"../dots-6.svg"} alt={""} height={18} width={18}/>
@@ -91,8 +104,8 @@ export default function Page(){
                         <Image src={"../plus-circle.svg"} alt={""} height={18} width={18} />
                         <div className="text-[12px] text-slate-50 leading-tight tracking-[-0.12px]">Create</div>
                     </div>
-                  </div>
-                  <div className="px-2 py-3 bg-gradient-to-b from-white/5 from-2% to-white/10 ring-1 ring-white/15  rounded-[8px] min-w-[270px] max-w-[270px]  flex flex-col gap-9 ">
+                        </div>
+                        <div className="px-2 py-3 bg-gradient-to-b from-white/5 from-2% to-white/10 ring-1 ring-white/15  rounded-[8px] min-w-[270px] max-w-[270px]  flex flex-col gap-9 ">
                     <div className="flex items-center justify-between shrink-0 ">
                         <div className=" inline-flex items-center gap-1 ">
                             <Image src={"../dots-6.svg"} alt={""} height={18} width={18}/>
@@ -104,7 +117,7 @@ export default function Page(){
                         <Image src={"../plus-circle.svg"} alt={""} height={18} width={18} />
                         <div className="text-[12px] text-slate-50 leading-tight tracking-[-0.12px]">Create</div>
                     </div>
-                  </div>
+                            </div>
                       </div>
                        <div className="flex flex-col items-start gap-3 mt-2 w-full">
                             <div className="text-[22px] font-semibold leading-normal text-white tracking-[-0.12px]">Status overview</div>
