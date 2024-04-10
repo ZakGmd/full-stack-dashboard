1e5b428afb68ba140d13a62a1bd1b48152de36bf
ZakGmd <Ghoumidatezakaria@gmail.com>
2024-04-10 16:24:31 +0000

    Working on accounts page

--- app/dashboard/accounts/page.tsx
+++ app/dashboard/accounts/page.tsx
@@ -1,7 +1,36 @@
-
+import Image from "next/image"
 export default function Accounts(){
 
     return (
-        <div>for accounts</div>
+        <div className="flex flex-col items-start">
+             <div className="flex items-center justify-between px-10 py-5 w-full border-b border-[#e6eff52d]">
+                <div className="text-[28px] font-semibold leading-normal text-white tracking-[-0.13px] ">Accounts</div>
+                <div className="flex items-center gap-7 shrink-0">
+                    <div className="w-[255px]  max-h-[50px] py-4 px-5 flex items-center gap-4 rounded-[40px] ring-white/15 ring-1 bg-gradient-to-b from-white/5 from-2% to-white/10 overflow-hidden shrink-0 ">
+                        <Image src={"../searchIcon.svg"} alt={"search icon blue thin"} width={20} height={20}/>
+                        <input type="text" name="search" id="searchInput" placeholder="Search for Something" className=" bg-transparent  ring-0 outline-none max-w-[176px] placeholder:text-[15px] placeholder:font-normal placeholder:text-white/20 text-white/40 placeholder:bg-transparent" />
+                    </div>
+                    <div className="flex w-[40px] h-[40px] items-center justify-center  ring-1 bg-gradient-to-br from-white/5 from-2% to-white/10 ring-white/15 shrink-0 rounded-full">
+                        <Image src={'../settings.svg'} alt={'settings icon blue thin'} width={20} height={20} className=" opacity-50 "  />
+                    </div>
+                    <div className="flex w-[40px] h-[40px] items-center justify-center  ring-1 bg-gradient-to-br from-white/5 from-2% to-white/10 ring-white/15 shrink-0 rounded-full">
+                        <Image src={'../notification.svg'} alt={'settings icon blue thin'} width={20} height={20} className=" opacity-50 " />
+                    </div>
+                    <div className="flex w-[55px] h-[55px] items-center justify-center  ring-1 bg-gradient-to-br from-white/5 from-2% to-white/10 ring-white/15 shrink-0 rounded-full overflow-hidden ">
+                        <Image src={'../avatrIcon.svg'} alt={''} width={55} height={55} className="object-contain h-auto w-auto"  />
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-start w-full py-5 px-10">
+                <div className="inline-flex items-center justify-between w-full">
+                    <div className="text-white text-[24px] leading-7 tracking-[-0.12px] font-medium">People and teams</div>
+                    <div className="flex items-center gap-4 shrink-0">
+                        <div className="px-3 py-1 bg-white/20 rounded-lg font-medium text-white cursor-pointer">Manage users</div>
+                        <div className="px-3 py-1 bg-white/20 rounded-lg font-medium text-white cursor-pointer">Create team</div>
+                        <div className="px-3 py-1 bg-blue-600 rounded-lg font-medium text-white cursor-pointer">Add people</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
