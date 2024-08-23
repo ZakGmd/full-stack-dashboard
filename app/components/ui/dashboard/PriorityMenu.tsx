f180191de86ff0841d313453e169b6a31d7488f9
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2024-08-23 01:34:12 +0100

    making a small changes to the colors of the priority popup

--- app/components/ui/dashboard/PriorityMenu.tsx
+++ app/components/ui/dashboard/PriorityMenu.tsx
@@ -24,14 +24,14 @@ export default function PriorityMenu({onPrioritySelect} : {onPrioritySelect : (s
   };
 
   return (
-    <div className={`pl-[4px] pr-[7px] items-center justify-center ${selectedValue === "Urgent" ? "bg-[#B13A4160] text-white hover:bg-[#B13A4180] hover:duration-200" : selectedValue === "Medium" ? "bg-[#4466FF60] text-white hover:bg-[#4466FF80] hover:duration-200 " : selectedValue === "High" ? "bg-[#CF940A60] text-white hover:bg-[#CF940A80] hover:duration-100" : "" } transition-all duration-300 ease-in flex gap-[6px] rounded-[4px] bg-white/20 cursor-pointer hover:bg-white/40  relative`} onClick={handleToggle}>
+    <div className={`pl-[4px] pr-[7px] items-center justify-center ${ selectedValue === "Urgent" ? "bg-[#B13A4145] text-white hover:bg-[#B13A4160] hover:duration-200" : selectedValue === "Medium" ? "bg-[#4466ff45] text-white hover:bg-[#4466FF55] hover:duration-200 " : selectedValue === "High" ? "bg-[#cf940a45] text-white hover:bg-[#CF940A55] hover:duration-100 hover:transition-colors" : "" } transition-colors duration-200 ease-out flex gap-[6px] rounded-[4px] bg-white/20 cursor-pointer hover:bg-white/40  relative`} onClick={handleToggle}>
       <Image alt="" height={14} width={14} src={selectedValue === "Urgent" ? "../flag-urgent.svg" : selectedValue === "High" ? "../flag-high.svg" : selectedValue === "Medium" ? "../flag-normal.svg" : selectedValue === "Low" ? "../flag-low.svg" : "../flag.svg"} /> 
       <div className="text-[12px] leading-[22px]">
         {selectedValue}
         {isOpen && (
 
           <motion.ul 
-            className="absolute  top-full left-0 mt-1 bg-gradient-to-b from-white/15 from-50% to-white/25 backdrop-blur-3xl backdrop-brightness-150 rounded-[4px] shadow-md border border-white/40 "
+            className="absolute  top-full left-0 mt-1 bg-black/50  backdrop-blur-3xl backdrop-brightness-150 rounded-[4px] shadow-md border border-white/15 "
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
            
@@ -43,7 +43,7 @@ export default function PriorityMenu({onPrioritySelect} : {onPrioritySelect : (s
             {options.map((option) => (
               <li
                 key={option.value}
-                className="pl-2 py-1 pr-[21px] gap-2 flex r text-[12px] leading-[20px] text-white hover:bg-black/50 hover:text-white hover:rounded-[4px] hover:transition duration-200 "
+                className="pl-2 py-1 pr-[21px] gap-2 flex r text-[12px] leading-[20px] text-white hover:bg-white/10 hover:text-white hover:rounded-[4px] hover:transition duration-200 "
                 onClick={() => handleSelect(option.value)}
               >
                 <Image src={option.icon} alt={''} height={9} width={9} />
